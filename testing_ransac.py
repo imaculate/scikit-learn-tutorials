2dfdb098917a9c29ddb487118a8ef88dae43fcf8
imaculate <imaculatemosha@yahoo.com>
2016-01-08 14:42:08 +0200

    test on ransac regressor passed

---
+++ testing_ransac.py
@@ -0,0 +1,10 @@
+import sklearn.linear_model as linear_model
+from sklearn import datasets
+import numpy as np
+
+ransac = linear_model.RANSACRegressor()
+digits = datasets.load_digits()
+X_digits = digits.data
+y_digits = digits.target
+ransac.fit(X_digits, y_digits, sample_weight=np.arange(y_digits.shape[0]))
+print(ransac._estimator_type)
