ac95cabe9b4e5c387a942057ea936debe3858466
imaculate <imaculatemosha@yahoo.com>
2016-01-22 18:57:39 +0200

    Minor changes

--- testing_ransac.py
+++ testing_ransac.py
@@ -1,10 +1,25 @@
 from sklearn import linear_model
 from sklearn import datasets
 import numpy as np
+from sklearn.utils.testing import *
+from numpy.testing import *
 
 ransac = linear_model.RANSACRegressor()
-digits = datasets.load_digits()
-X_digits = digits.data
-y_digits = digits.target
-ransac.fit(X_digits, y_digits, sample_weight=np.arange(y_digits.shape[0]))
-print(ransac._estimator_type)
+digits = datasets.load_diabetes()
+X = digits.data
+y = digits.target
+n_samples = y.shape[0]
+
+
+weights = np.ones(n_samples)
+ransac.fit(X, y, weights)
+
+print(ransac.inlier_mask_.shape)
+assert_equal(ransac.inlier_mask_.shape[0], n_samples)
+
+
+ransac2 = linear_model.RANSACRegressor(linear_model.Lasso())
+
+assert_raises(TypeError,ransac2.fit,X,y, weights )
+
+
